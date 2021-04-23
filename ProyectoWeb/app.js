54d3e998c13f436d7f19a39a43bd5fa3d3ae38d6
Javi-GR <74304704+Javi-GR@users.noreply.github.com>
2021-04-23 23:17:01 +0200

    Cambio a responsive, inicio.html, contacto.html
    Falta cambiar proyectos.html y login

--- ProyectoWeb/app.js
+++ ProyectoWeb/app.js
@@ -1,14 +1,3 @@
-/*Anima el timeline de las letras iniciales*/
-
-const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
-
-tl.to('.texto', { y: "0%", duration: 1, stagger: 0.25 });
-tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
-tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
-tl.fromTo("nav", {opacity: 0},{opacity: 1 , duration: 1});
-
-tl.fromTo(".texto-grande", {opacity: 0},{opacity: 1 , duration: 1});
-
 /* Funcion que activa la animacion de aparicion y desaparicion del menu de navegacion en formato movil
 */
 const navSlide = () => {
@@ -21,3 +10,15 @@ const navSlide = () => {
 }
 
 navSlide();
+
+/*Anima el timeline de las letras iniciales*/
+
+const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
+
+tl.to('.texto', { y: "0%", duration: 1, stagger: 0.25 });
+tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
+tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
+tl.fromTo("nav", {opacity: 0},{opacity: 1 , duration: 1});
+
+tl.fromTo(".texto-grande", {opacity: 0},{opacity: 1 , duration: 1});
+
